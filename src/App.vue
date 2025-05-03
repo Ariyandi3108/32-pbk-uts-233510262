20b99dd1acd5a7b1cb78a4521ffeb43f3f364478
Ariyandi3108 <Ariyandi31>
2025-05-03 19:22:48 +0700

    addTask

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,34 @@
 <script setup>
 import { ref } from 'vue'
 
-const tasks = ref([
-  {
-    id: 1,
-    title: 'Task 1',
-    completed: false
-  },
-  {
-    id: 2,
-    title: 'Task 2',
-    completed: true
-  },
-  {
-    id: 3,
-    title: 'Task 3',
-    completed: false
+const tasks = ref([])
+const newTask = ref('')
+
+const addTask = () => {
+  if (newTask.value !== '') {
+    tasks.value.push({
+      id: Date.now(),
+      title: newTask.value,
+      completed: false
+    })
   }
-])
+  newTask.value = ''
+}
 
 </script>
 
 <template>
   <div>
+    <input type="text" v-model="newTask" @keyup.enter="addTask">
+    <button @click="addTask">Add Task</button>
+
     <ul>
       <li v-for="task in tasks" :key="task.id">
-        {{  task.title }} - {{ task.completed ? 'Completed' : 'Not Completed' }}
+        {{ task.title }}
       </li>
     </ul>
   </div>
- 
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
